c5ebb154ffcdbc79b5d2fa7ba40fd7c451c3c99c
Abel <sdwyub@qq.com>
2016-02-18 11:41:55 +0800

    login view over

--- src/js/view/login.js
+++ src/js/view/login.js
@@ -5,6 +5,9 @@
  */
 ;(function(){
 	App.loginView.wrap({
+		events:{
+			"tap .login-btn":"login"
+		},
 		beforeRender:function(){
 			//TODO
 			return true;
@@ -13,6 +16,17 @@
 			//TODO
 		},
 		afterUnRender:function(){
+		},
+		login:function(){
+			var name=this.$el.find("[type=text]").val();
+			var passwd=this.$el.find("[type=password]").val();
+
+			if(name&&passwd){
+				alert("开始登录处理");
+				App.loading();
+			}else{
+				alert("用户名或者密码不能为空！");
+			}
 		}
 	});
 })();
