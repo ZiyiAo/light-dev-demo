e7f18ceb31e9d90ae562c783e5f30601187cea1b
Abel <sdwyub@qq.com>
2016-03-18 10:52:41 +0800

    增加f7框架支持

--- src/js/api.js
+++ src/js/api.js
@@ -1,16 +1,4 @@
 ;(function () {
 
-  app.service('API',function ($http) {
-    
-
-    function _execute(func,params,callback){
-      $http({
-        method:'GET',
-        url:prefix+func+".do",
-        params:angular.extend(params,{
-          authUserId:14717
-        })
-      }).success(callback);
-    }
-  })
+  
 })();
--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,27 @@
 ;(function(){
-    var app = angular.module('app', ['ngRoute'])
+    Template7.registerHelper('json_stringify', function (context) {
+        return JSON.stringify(context);
+    });
 
-    app.config(function ($routeProvider){
-        $routeProvider
-            .when('/', {
-                controller: 'index',
-                templateUrl: 'view/index.html'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-    })
+    // Initialize your app
+    var app = new Framework7({
+        animateNavBackIcon: true,
+        // Enable templates auto precompilation
+        precompileTemplates: true,
+        // Enabled pages rendering using Template7
+        template7Pages: true,
+        // Specify Template7 data for pages
+        template7Data: {}
+    });
 
+    // Export selectors engine
+    var $$ = Dom7;
+
+    // Add main View
+    var mainView = app.addView('.view-main', {
+        // Enable dynamic Navbar
+        dynamicNavbar: true,
+    });
     window.app = app;
 })();
 
